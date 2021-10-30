70374633c031386eb78ffd36fd4ad3dee94dfb97
Michele Perrone <perrone.michele@outlook.com>
2021-10-30 13:37:31 +0200

    Delete the template code and make CudeIDE generate the working CDC code

--- src/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h
+++ src/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h
@@ -1,8 +1,8 @@
 /**
   ******************************************************************************
-  * @file    usbd_template_core.h
+  * @file    usbd_cdc.h
   * @author  MCD Application Team
-  * @brief   Header file for the usbd_template_core.c file.
+  * @brief   header file for the usbd_cdc.c file.
   ******************************************************************************
   * @attention
   *
@@ -18,8 +18,8 @@
   */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __USB_TEMPLATE_CORE_H
-#define __USB_TEMPLATE_CORE_H
+#ifndef __USB_CDC_H
+#define __USB_CDC_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,19 +32,51 @@ extern "C" {
   * @{
   */
 
-/** @defgroup USBD_TEMPLATE
-  * @brief This file is the header file for usbd_template_core.c
+/** @defgroup usbd_cdc
+  * @brief This file is the Header file for usbd_cdc.c
   * @{
   */
 
 
-/** @defgroup USBD_TEMPLATE_Exported_Defines
+/** @defgroup usbd_cdc_Exported_Defines
   * @{
   */
-#define TEMPLATE_EPIN_ADDR                 0x81
-#define TEMPLATE_EPIN_SIZE                 0x10
+#define CDC_IN_EP                                   0x81U  /* EP1 for data IN */
+#define CDC_OUT_EP                                  0x01U  /* EP1 for data OUT */
+#define CDC_CMD_EP                                  0x82U  /* EP2 for CDC commands */
 
-#define USB_TEMPLATE_CONFIG_DESC_SIZ       64
+#ifndef CDC_HS_BINTERVAL
+#define CDC_HS_BINTERVAL                          0x10U
+#endif /* CDC_HS_BINTERVAL */
+
+#ifndef CDC_FS_BINTERVAL
+#define CDC_FS_BINTERVAL                          0x10U
+#endif /* CDC_FS_BINTERVAL */
+
+/* CDC Endpoints parameters: you can fine tune these values depending on the needed baudrates and performance. */
+#define CDC_DATA_HS_MAX_PACKET_SIZE                 512U  /* Endpoint IN & OUT Packet size */
+#define CDC_DATA_FS_MAX_PACKET_SIZE                 64U  /* Endpoint IN & OUT Packet size */
+#define CDC_CMD_PACKET_SIZE                         8U  /* Control Endpoint Packet size */
+
+#define USB_CDC_CONFIG_DESC_SIZ                     67U
+#define CDC_DATA_HS_IN_PACKET_SIZE                  CDC_DATA_HS_MAX_PACKET_SIZE
+#define CDC_DATA_HS_OUT_PACKET_SIZE                 CDC_DATA_HS_MAX_PACKET_SIZE
+
+#define CDC_DATA_FS_IN_PACKET_SIZE                  CDC_DATA_FS_MAX_PACKET_SIZE
+#define CDC_DATA_FS_OUT_PACKET_SIZE                 CDC_DATA_FS_MAX_PACKET_SIZE
+
+/*---------------------------------------------------------------------*/
+/*  CDC definitions                                                    */
+/*---------------------------------------------------------------------*/
+#define CDC_SEND_ENCAPSULATED_COMMAND               0x00U
+#define CDC_GET_ENCAPSULATED_RESPONSE               0x01U
+#define CDC_SET_COMM_FEATURE                        0x02U
+#define CDC_GET_COMM_FEATURE                        0x03U
+#define CDC_CLEAR_COMM_FEATURE                      0x04U
+#define CDC_SET_LINE_CODING                         0x20U
+#define CDC_GET_LINE_CODING                         0x21U
+#define CDC_SET_CONTROL_LINE_STATE                  0x22U
+#define CDC_SEND_BREAK                              0x23U
 
 /**
   * @}
@@ -58,6 +90,38 @@ extern "C" {
 /**
   * @}
   */
+typedef struct
+{
+  uint32_t bitrate;
+  uint8_t  format;
+  uint8_t  paritytype;
+  uint8_t  datatype;
+} USBD_CDC_LineCodingTypeDef;
+
+typedef struct _USBD_CDC_Itf
+{
+  int8_t (* Init)(void);
+  int8_t (* DeInit)(void);
+  int8_t (* Control)(uint8_t cmd, uint8_t *pbuf, uint16_t length);
+  int8_t (* Receive)(uint8_t *Buf, uint32_t *Len);
+
+} USBD_CDC_ItfTypeDef;
+
+
+typedef struct
+{
+  uint32_t data[CDC_DATA_HS_MAX_PACKET_SIZE / 4U];      /* Force 32bits alignment */
+  uint8_t  CmdOpCode;
+  uint8_t  CmdLength;
+  uint8_t  *RxBuffer;
+  uint8_t  *TxBuffer;
+  uint32_t RxLength;
+  uint32_t TxLength;
+
+  __IO uint32_t TxState;
+  __IO uint32_t RxState;
+}
+USBD_CDC_HandleTypeDef;
 
 
 
@@ -73,7 +137,8 @@ extern "C" {
   * @{
   */
 
-extern USBD_ClassTypeDef  USBD_TEMPLATE_ClassDriver;
+extern USBD_ClassTypeDef  USBD_CDC;
+#define USBD_CDC_CLASS    &USBD_CDC
 /**
   * @}
   */
@@ -81,6 +146,19 @@ extern USBD_ClassTypeDef  USBD_TEMPLATE_ClassDriver;
 /** @defgroup USB_CORE_Exported_Functions
   * @{
   */
+uint8_t  USBD_CDC_RegisterInterface(USBD_HandleTypeDef   *pdev,
+                                    USBD_CDC_ItfTypeDef *fops);
+
+uint8_t  USBD_CDC_SetTxBuffer(USBD_HandleTypeDef   *pdev,
+                              uint8_t  *pbuff,
+                              uint16_t length);
+
+uint8_t  USBD_CDC_SetRxBuffer(USBD_HandleTypeDef   *pdev,
+                              uint8_t  *pbuff);
+
+uint8_t  USBD_CDC_ReceivePacket(USBD_HandleTypeDef *pdev);
+
+uint8_t  USBD_CDC_TransmitPacket(USBD_HandleTypeDef *pdev);
 /**
   * @}
   */
@@ -89,7 +167,7 @@ extern USBD_ClassTypeDef  USBD_TEMPLATE_ClassDriver;
 }
 #endif
 
-#endif  /* __USB_TEMPLATE_CORE_H */
+#endif  /* __USB_CDC_H */
 /**
   * @}
   */
